678bcebd9862de794b8a058ba35eb83541f744f4
achamber <achamber@c7eeca16-736b-6919-ab84-0fdfb1d6aabd>
2010-12-07 03:06:10 +0000

    Fixed problems with not pre-multiplying point sets by K1 and K2

--- simulator/generatePoints.m
+++ simulator/generatePoints.m
@@ -3,12 +3,23 @@ function [ pointList ] = generatePoints( )
 %   Detailed explanation goes here
 
 % A very simple point list to start
-[X, Y, Z] = ndgrid(-0.5:0.1:0.5, -0.5:0.5:0.5, 0);%, -0.5:0.5:0.5);
-pointList = [X(:), Y(:), Z(:)];
+% [X, Y, Z] = ndgrid(-0.5:0.1:0.5, -0.5:0.5:0.5, 0);%, -0.5:0.5:0.5);
+% pointList = [X(:), Y(:), Z(:)];
+
+% A cube:
+pointList = [   -0.5, -0.5, -0.5;
+                -0.5, -0.5,  0.5;
+                -0.5,  0.5, -0.5;
+                -0.5,  0.5,  0.5;
+                 0.5, -0.5, -0.5;
+                 0.5, -0.5,  0.5;
+                 0.5,  0.5, -0.5;
+                 0.5,  0.5,  0.5];
 
 % DEBUG: View points
 figure, plot3(pointList(:,1), pointList(:,2), pointList(:,3), '.'); 
 title('Original 3D points');
+axis equal;
 
 end
 
