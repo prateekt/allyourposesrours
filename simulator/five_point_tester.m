42e46c717a11d5e757deeec75fc3e7a523e9011e
achamber <achamber@c7eeca16-736b-6919-ab84-0fdfb1d6aabd>
2010-12-07 13:27:40 +0000

    Point cloud is now a cube. We check for error in q'*E*p = 0 and pick the E with the lowest sum error. This E looks great for Nister's and our 5-point implementation.

--- simulator/five_point_tester.m
+++ simulator/five_point_tester.m
@@ -21,8 +21,16 @@ load('templeSparseRing_data.mat');
 h_pts1 = [pts1 ones(size(pts1,1),1)];
 h_pts2 = [pts2 ones(size(pts2,1),1)];
 
+%% Premultiply the input points by inv(K1) and inv(K2)
+h_pts1 = K{1} \ h_pts1';
+h_pts2 = K{2} \ h_pts2';
+h_pts1 = h_pts1';
+h_pts2 = h_pts2';
 
 %% Perfect backprojection original projection matrices
+% backprojected points do not need to be premultiplied by K1 and K2 since
+% the function backproject makes use of the entire projection matrix which
+% includes camera intrinsics and extrinsics.
 [ idealReprojPoints ] = backproject( pts1, pts2, P{1}, P{2} );
 figure();
 plot3(idealReprojPoints(:,1), idealReprojPoints(:,2), idealReprojPoints(:,3), '.');
@@ -32,18 +40,6 @@ axis equal;
 %% Backprojection with error-free E and calculated projection matrices
 E = findGndTruthE(R{1}, t{1}, R{2}, t{2});
 reprojPoints = triangulateWithE(pts1, pts2, E', K{1}, K{2});
-figure()
-plot3(reprojPoints(:,1), reprojPoints(:,2), reprojPoints(:,3), '.');
-title('Gnd truth E, approx projection matrices');
-axis equal;
-
-
-%% Premultiply the input points by inv(K1) and inv(K2)
-h_pts1 = K{1} \ h_pts1';
-h_pts2 = K{2} \ h_pts2';
-h_pts1 = h_pts1';
-h_pts2 = h_pts2';
-
 
 %% Nister's 5-point implementation
 % Evec = calibrated_fivepoint(h_pts2', h_pts1');
@@ -55,32 +51,34 @@ h_pts2 = h_pts2';
 % end
 
 
-%% Calculate the essential matrix with 5-point algorithm
+%% Our 5-point implementation
 
  EList = five_point(h_pts1, h_pts2);
 
-%% Backproject the points into 3D and view them
-for i = 1:length(EList)
-    
-    reprojPoints = triangulateWithE(pts1, pts2, EList{i}', K{1}, K{2});
-    figure()
-    plot3(reprojPoints(:,1), reprojPoints(:,2), reprojPoints(:,3), '.');
-    axis equal
-    
-    
-end
 
 %% Check error
+
+minSumError = inf;
+
 for i=1:length(EList)
-  i
   E = EList{i};
   % Check determinant constraint! 
-  error_determinate = det( E)
+  error_determinate = det( E);
   % Check trace constraint
-  error_trace = 2 *E*transpose(E)*E -trace( E*transpose(E))*E
+  error_trace = 2 *E*transpose(E)*E -trace( E*transpose(E))*E;
   % Check reprojection errors
-  error_reprojection = diag( h_pts2 * E * h_pts1')
-  sum_error_proj = sum(abs(error_reprojection))
+  error_reprojection = diag( h_pts2 * E * h_pts1');
+  sum_error_proj = sum(abs(error_reprojection));
+  
+  % Find E with the smallest error
+  if (sum_error_proj < minSumError)
+      minSumError = sum_error_proj;
+      bestE = E;
+  end
+  
 end
+
+% Show the points in 3D backprojected with the best E.
+triangulateWithE(pts1, pts2, bestE', K{1}, K{2});
 
 
